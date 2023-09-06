9c03ce0f3a9e8bd6cf846192b5802451dbed4a3d
Arviaardhan <arviaardhan@gmail.com>
2023-09-06 14:19:43 +0700

    style : [AF] add Rating Bar

--- lib/Movie.dart
+++ lib/Movie.dart
@@ -1,12 +1,14 @@
+import 'dart:ffi';
+
 class Movie{
   late String gambar;
   late String judul;
   late String rilis;
   late String durasi;
   late String genre;
-  late String rating;
+  late double rating;
 
-  Movie(String gambar, String judul, String rilis, String durasi, String genre, String rating){
+  Movie(String gambar, String judul, String rilis, String durasi, String genre, double rating){
     this.gambar = gambar;
     this.judul = judul;
     this.rilis = rilis;
